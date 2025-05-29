39150c485c5b5dc1c91c674d008f2bd304d6964f
Michael Lambert <mlambert125@live.com>
2025-05-29 13:30:35 -0400

    ...

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "html",
+                "vim",
+                "rust",
+                "bash",
+                "c",
+                "javascript",
+                "json",
+                "jsdoc",
+                "lua",
+                "markdown",
+                "python",
+                "toml",
+            },
+        },
+    },
+}
